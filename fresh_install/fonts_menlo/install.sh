78d0687ebba31a1a8e034c52f5df84f3601632f5
Huangshan Chen <chen@kip.uni-heidelberg.de>
2018-03-17 19:29:58 +0100

    add a nice powerline font - Menlo

---
+++ fresh_install/fonts_menlo/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## adapted from the installation script for the powerlint fonts
+if test "$(uname)" = "Darwin" ; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+  mkdir -p $font_dir
+fi
+
+# Copy all fonts to user fonts directory
+echo "Copying fonts..."
+cp Menlo* $font_dir/
+
+# Reset font cache on Linux
+if which fc-cache >/dev/null 2>&1 ; then
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -f "$font_dir"
+fi
+
+echo "Fonts installed to $font_dir"
