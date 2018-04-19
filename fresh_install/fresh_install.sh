0892904d220d35f28d56f1ad6c3c2c8f74a690a2
Huangshan Chen <chen@kip.uni-heidelberg.de>
2018-04-19 23:52:00 +0200

    fresh_install.sh: check if the files exist before apply changes
    and other minor changes

--- fresh_install/fresh_install.sh
+++ fresh_install/fresh_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ### install debian packages
-sudo apt install vim neovim tmux git git-gui gitk
-
+sudo apt install vim neovim tmux git git-gui gitk dconf-cli
+sudo apt install bash-completion exuberant-ctags
+sudo apt install python3 python3-pip
 
 ### show user name in the lightdm greeter
 echo "Enable the user in the lightdm greeter..."
 sudo cp config_files/02_enable_users.conf /usr/share/lightdm/lightdm.conf.d/
 echo "Done."
-
 
 ### apply the dot files
 echo "Applying the dot files..."
@@ -17,30 +17,56 @@ cp ../dotfile/dot_vimrc ~/.vimrc
 cp ../dotfile/dot_tmux.conf ~/.tmux.conf
 cp ../dotfile/dot_screenrc ~/.screenrc
 cp ../dotfile/dot_bash_functions ~/.bash_functions
+source ~/.bashrc
 echo "Done."
 
+### create configuration for nvim
+if [ ! -f ~/.config/nvim/init.vim ]; then
+	echo "Create Nvim configurations..."
+	mkdir -p ~/.config/nvim
+	echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc" > ~/.config/nvim/init.vim
+	echo "Done."
+fi
 
 ### install menlo fonts
-echo "Install Menlo fonts..."
-pushd .
-cd fonts_menlo/
-./install.sh
-popd
-echo "Done."
+for f in ~/.local/share/fonts/Menlo_*; do
+	if [ ! -e $f ]; then
+		echo "Install Menlo fonts..."
+		pushd .
+		cd fonts_menlo/
+		./install.sh
+		popd
+		echo "Done."
+	fi
+	break
+done
 
+### install Vundle plugin for organizing Vim Plugins
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+	echo "Install Vim Vundle..."
+	mkdir -p ~/.vim/bundle/
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	echo "Done."
+fi
 
-### For Vim Plugins
-echo "Install Vim Vundle..."
-mkdir -p ~/.vim/bundle/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Done."
+### setup nvim
+sudo pip3 install neovim
+nvim +PluginInstall +UpdateRemotePlugins +qall
 
-echo "Install gnome-terminal solarized color"
-pushd .
-mkdir -p ~/git-repos/
-cd ~/git-repos/
-git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized/
-./install.sh
-popd
-echo "Done."
+### apply soloarized color scheme to gnome-terminal
+gitRepoDir=../../
+if [ ! -e $gitRepoDir/gnome-terminal-colors-solarized/ ]; then
+	echo "Install gnome-terminal solarized color"
+	echo "Better create a new gnome-terminal profile before installation"
+	pushd .
+	mkdir -p $gitRepoDir
+	cd $gitRepoDir
+	git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+	cd gnome-terminal-colors-solarized/
+	./install.sh
+	popd
+	echo "Done."
+fi
+unset gitRepoDir
